4dfdb627bfac5ab89182c4f40d9a05daa29f8a24
alwzsoft <alwzsoft@gmail.com>
2026-01-19 15:22:29 +0000

    Update from https://github.com/alwzsoft/source_only/commit/2091c0fed74eede5254ce8594a2da297e3dba598

---
+++ data/pictures.js
@@ -0,0 +1,13 @@
+const pictures = [
+  {
+    img: "images/20260120_001841167.jpg",
+    date: "2026-01-20"
+  }
+];
+
+const pictures = [
+  {
+    img: "images/2026-01-18 130616.png",
+    date: "2026-01-18"
+  }
+];
